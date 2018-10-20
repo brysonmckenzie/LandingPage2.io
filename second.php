a45adf2ad1b1c2ce81e2d65fa148d8d5a3bc607d
brysonmckenzie <brysonmckenzie83@gmail.com>
2018-10-20 14:16:06 -0700

    updates

---
+++ second.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?PHP
+	if(isset($_POST['goto'])){
+			echo '<script type="text/javascript">
+				   window.location = "https://www.ebay.com"
+			  </script>';
+	?>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
